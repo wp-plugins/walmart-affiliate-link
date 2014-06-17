4d854fe79f902d2adb9ff0f08a7fe04302365ca4
tofa.wl <tofa.wl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-17 06:07:00 +0000

    Remove google analytics.
    git-svn-id: https://plugins.svn.wordpress.org/walmart-affiliate-link/trunk@933478 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- regex_matcher.php
+++ regex_matcher.php
@@ -9,8 +9,6 @@ function generate_links( $content ) {
     return $content;
   }
 
-  global $link_created;
-
   // g isn't required in the regex options because preg_replace already
   // replaces all occurances in the text.
   /* 
@@ -33,8 +31,6 @@ function generate_links( $content ) {
   $newcontent = preg_replace_callback(
     $pattern_search,
     function($matches) {
-               global $link_created;
-               $link_created = 1;
                global $wmt_options;
                return '<a target="_blank" href="http://linksynergy.walmart.com/fs-bin/click?id='.$wmt_options['encrypted'].'&offerid=223073.7500&type=4&subid=0&tmpid=1081&RD_PARM1=http%3A%2F%2Fwww.walmart.com%2Fsearch%2Fsearch-ng.do%3Fsearch_query%3D'.urlencode($matches[1]).'">'.$matches[1].'</a>';
     },
@@ -43,8 +39,6 @@ function generate_links( $content ) {
   $newcontent = preg_replace_callback(
     $pattern_link,
     function($matches) {
-               global $link_created;
-               $link_created = 1;
                global $wmt_options;
                $tmpid = "";
                if (strpos($matches[1], '?')) {
@@ -61,22 +55,11 @@ function generate_links( $content ) {
   $newcontent = preg_replace_callback(
     $pattern_banner,
     function($matches) {
-               global $link_created;
-               $link_created = 1;
                global $wmt_options;
                return '<iframe target="_blank" height="250" width="300" src="http://affil.walmart.com/ad?wtype=rc&lsnTrack=http%3A%2F%2Flinksynergy.walmart.com%2Ffs-bin%2Fclick%3Fid%3D'.$wmt_options['encrypted'].'%26offerid%3D223073.7503%26type%3D13%26subid%3D0%26tmpid%3D1081&exp=ID&itemIDs='.$matches[1].'"></iframe>';
     },
     $newcontent);
-  // Collecting some data about the usage of the plugin development purposes.
-  try {
-    if ($link_created) {
-        file_get_contents("http://www.google-analytics.com/collect?v=1&tid=UA-51915563-1&dl=http%3A%2F%2Faffil.walmart.com&t=pageview&dp=regex-used&cid=".rand(0,100000000));
-    } else {
-        file_get_contents("http://www.google-analytics.com/collect?v=1&tid=UA-51915563-1&dl=http%3A%2F%2Faffil.walmart.com&t=pageview&dp=regex-unused&cid=".rand(0,100000000));
-    }
-  } catch (Exception $e) {
-      echo 'Caught exception: ',  $e->getMessage(), "\n";
-  }
+
   return $newcontent;
 }
 
--- wmt_admin_page.php
+++ wmt_admin_page.php
@@ -113,16 +113,6 @@ Congratulations! you are just one step away from using this plugin. This plugin 
   </div>
 </div>
 </div>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-51915563-1', 'affil.walmart.com');
-  ga('send', 'pageview');
-
-</script>
 <?php
   echo ob_get_clean();
 }
